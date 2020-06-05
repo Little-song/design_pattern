d5106ef276d2f5e096b2625ebe86141581341663
Little-song <879499473@qq.com>
2020-06-05 16:56:49 +0800

    SimpleFactory finished

--- SimpleFactory/handle.php
+++ SimpleFactory/handle.php
@@ -7,11 +7,13 @@ require "core/Factory.php";
 
 use SimpleFactory\Factory;
 
+// 获取参数和操作符
 $num1 = $_POST['num1'];
 $num2 = $_POST['num2'];
 $operator = $_POST['operator'];
-
+// 实例化工厂
 $factory = new Factory();
+// 传入操作符，创建对应计算类
 $operation = $factory->createOperation($operator);
-
+// 输出结果
 echo $operation->calculate($num1, $num2);
--- SimpleFactory/index.php
+++ SimpleFactory/index.php
@@ -7,24 +7,25 @@
 </head>
 <body>
     <form action="" >
+        <!-- 输入值1 -->
         <label for="num1">值1：</label>
         <input id="num1" name="num1" type="text"><br/><br/>
-
+        <!-- 输入运算符 -->
         <label for="operator">运算符：</label
         ><input id="operator" name="operator" type="text"><br/><br/>
-
+        <!-- 输入值2 -->
         <label for="num2">值2：</label>
         <input id="num2" name="num2" type="text"><br/><br/>
-
+        <!-- 计算结果 -->
         <div class="result">结果是：<span></span></div><br/>
-
+        <!-- 计算按钮 -->
         <button type="button">计算</button>
-        
     </form>
 
     <script src="https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.js"></script>
 
     <script>
+        // 点击发起运算请求
         $('button').on('click',function(){
             var form = document.querySelector("form");
             var data = new FormData(form);
@@ -36,6 +37,7 @@
                 processData: false,
                 contentType: false,
                 success: function(res){
+                    // 成功返回结果处理
                     $('.result span').text(res)
                 }
             })
